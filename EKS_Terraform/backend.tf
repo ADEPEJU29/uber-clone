915bd2fc2eca177640139352535854a1c2f85a50
ADEPEJU29 <166997144+ADEPEJU29@users.noreply.github.com>
2024-07-19 11:35:08 -0400

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "adepeju-backups" # Replace with your actual S3 bucket name
+    bucket = "adepeju-backup" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
